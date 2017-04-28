341cd8d3fe4eaf0f93132a781eda32a82f50fb7a
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-04-27 23:21:01 -0400

    Add migration files I'm not sure why the original migration files were added

--- db/migrate/20170427152910_entry.rb
+++ db/migrate/20170427152910_entry.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Entry < ActiveRecord::Migration[5.0]
   def change
   end
---
+++ db/migrate/20170428024913_add_user_to_entries.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddUserToEntries < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :entries, :user, foreign_key: true
+  end
+end
