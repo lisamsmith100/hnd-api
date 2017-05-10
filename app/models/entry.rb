3c9eb5e43e2e4ccd263816df1f6be1418f9e6806
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-05-09 20:13:38 -0400

    Update validation for create only title attribute
    also remigrated just in case

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 class Entry < ApplicationRecord
   belongs_to :user
   # , class_name: 'User', foreign_key: 'user_id'
   # validations
-  validates :title, presence: true
+  validates :title, :presence => true, :on => :create
   validates :user_id, presence: true
 end
 #
---
+++ db/migrate/20170509200238_migration.rb
@@ -0,0 +1,4 @@
+class Migration < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
